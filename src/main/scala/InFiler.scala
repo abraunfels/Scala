0a67cf6449f4d34dfb27afb1e700aff3b5aff228
abraunfels <anastasia.braunfels@gmail.com>
2019-07-23 23:04:24 +0300

    v.0.3

--- src/main/scala/InFiler.scala
+++ src/main/scala/InFiler.scala
@@ -5,7 +5,8 @@ import scala.collection._
 trait InFiler {
     val fileName: String
     val defaultBlockSize = 256 * 1024 //256Kb
-    val totalChunks = getNumberOfChunks
+
+    private[this] var alreadyReadenBytes = 0
 
     private def getNumberOfChunks: Int = {
       val randomAccessFile = new RandomAccessFile(fileName, "r")
@@ -15,15 +16,31 @@ trait InFiler {
         randomAccessFile.close
       }
     }
-    def readLines(chunkIndex: Int, offset: Int): Array[String] = {
+
+    def checkFinish: Boolean = {
+      var flagFinish : Boolean = false
+      val randomAccessFile = new RandomAccessFile(fileName, "r")
+      try {
+        val temp = randomAccessFile.length
+        if (alreadyReadenBytes == randomAccessFile.length) {
+          flagFinish = true
+        }
+        flagFinish
+      } finally {
+        randomAccessFile.close
+      }
+    }
+    def readLines(): Array[String] = {
       val randomAccessFile = new RandomAccessFile(fileName, "r")
       try {
         val byteBuffer = Array.ofDim[Byte](defaultBlockSize)
-        var seek = (chunkIndex - 1) * defaultBlockSize - offset
-        randomAccessFile.seek(seek)
+        randomAccessFile.seek(alreadyReadenBytes)
         val len = randomAccessFile.read(byteBuffer)
+        alreadyReadenBytes += len
         val rawString = new String(byteBuffer, 0, len)
-        rawString.split(Properties.lineSeparator)
+        val Lines = rawString.split(Properties.lineSeparator)
+        if (rawString.last != '\n') alreadyReadenBytes -= Lines.last.length
+        Lines
       } finally {
         randomAccessFile.close
       }
--- src/main/scala/Processor.scala
+++ src/main/scala/Processor.scala
@@ -2,23 +2,18 @@ import scala.collection._
 import scala.util.matching.Regex
 
 class Processor (sourceFile: String) extends {val fileName = sourceFile} with InFiler{
-  var counter: Int = 0
   def processFile: mutable.HashMap [String, PayStats] = {
     var resStat = new mutable.HashMap [String, PayStats]
     val startTime = System.currentTimeMillis
     var offset: Int = 0
     var j: Int = 0
-    for (i <- 1 to totalChunks+1) {
+    while (!checkFinish){
       var tmp = Array[String]()
-      println (offset)
-      tmp = readLines(i, offset)
-      offset = tmp(tmp.size - 1).size
-      println(tmp(0)+" "+ tmp(tmp.size - 1))
+      tmp = readLines()
       processLines (tmp, resStat)
     }
     val endTime = System.currentTimeMillis
     println("Total time in millis: " + (endTime - startTime))
-    println (counter)
     resStat
   }
 
@@ -27,7 +22,6 @@ class Processor (sourceFile: String) extends {val fileName = sourceFile} with In
       val resStrArr = elSrcArr.split(";")
       val pattern = new Regex("\\.[0-9][0-9]")
       if ((resStrArr.size == 4) && (pattern.findFirstIn(resStrArr(3)).nonEmpty)) {
-        counter += 1
         if (statMap.contains(resStrArr(2))) {
           statMap(resStrArr(2)).addPayt(resStrArr(3).toFloat, resStrArr(1))
         }
