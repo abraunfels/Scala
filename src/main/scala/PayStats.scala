a7bdc43123f67c4a40c9dbc289ae3eba4903356a
abraunfels <anastasia.braunfels@gmail.com>
2019-07-24 14:43:05 +0300

    v.1

--- src/main/scala/PayStats.scala
+++ src/main/scala/PayStats.scala
@@ -6,8 +6,9 @@ class PayStats (val operator: String, var sum: Float, date: String){ //это г
   var daily = new mutable.HashMap [String, Tuple2[Float, Int]]
   daily (date) = Tuple2(sum, 1)
 
-  //var maxPays  = new mutable.MutableList[Float]
-  //maxPays += sum
+  val quantityMaxPays = 5
+  var maxPays: List[Float] = List()
+  maxPays.::(sum)
   //конец главного конструктора
 
   def addPayt (amount: Float, date: String){
@@ -19,9 +20,28 @@ class PayStats (val operator: String, var sum: Float, date: String){ //это г
     else {
       daily(date) = Tuple2 (amount, 1)
     }
-    /*if (maxPays.size <= 5){
+    maxPays = addToList(amount, maxPays)
+  }
 
-    }*/
+  private def addToList(elem : Float, list: List[Float]): List[Float] ={
+    var resList : List [Float] = List() //вот как обойтись без такой штуки??
+    if (list.isEmpty){
+      resList = List(elem)
+    }
+    if ((list.size <= quantityMaxPays) && (!list.isEmpty)){
+      if (elem < list.head){
+        resList = elem :: list
+      }
+      if (elem > list.head){
+        resList = list.head :: addToList(elem, list.tail)
+      }
+      if (elem == list.head)
+        resList = list
+    }
+    if (resList.size > quantityMaxPays){
+      resList = resList.tail
+    }
+    resList
   }
 
   def display: Unit ={
@@ -29,8 +49,8 @@ class PayStats (val operator: String, var sum: Float, date: String){ //это г
     println ("Sum of payments: " + sum)
     println ("Quantity of paymetns: " + quantity)
     print ("Top-5 payments")
-    //for (el <- maxPays)
-    //  print(" " + el)
+    for (el <- maxPays)
+      print(" " + el)
     printf("\n")
     println("Daily average")
     for ((k, v) <- daily)
--- src/main/scala/TaskOne.scala
+++ src/main/scala/TaskOne.scala
@@ -3,7 +3,6 @@ object TaskOne extends App {
   val test = new Processor("txs.csv")
   val res = test.processFile
   for ((k, v) <- res)
-   v.display
-
+    v.display
 }
 
